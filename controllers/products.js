b76d40be13b7392cd9729421bb2458ed6f62bdb9
elshora97 <A.elshora@dataserve.com.sa>
2024-02-28 00:53:08 +0200

    Added product filter list

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,28 @@
-const getAllProductsStatic = (req, res) => {
-  res.status(200).json({ msg: "products testing route" });
+const product = require("../models/product");
+
+const getAllProductsStatic = async (req, res) => {
+  const products = await product.find({});
+  res.status(200).json({ noHits: products.length, products });
 };
 
-const getAllProducts = (req, res) => {
-  res.status(200).json({ msg: "products route" });
+const getAllProducts = async (req, res) => {
+  const { featured, company, name } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured;
+  }
+
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  const products = await product.find(queryObject);
+  res.status(200).json({ noHits: products.length, products });
 };
 
 module.exports = { getAllProductsStatic, getAllProducts };
