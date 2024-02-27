a732ed7ce07d739aa5b7f243a0687cb8a77cbeb3
elshora97 <A.elshora@dataserve.com.sa>
2024-02-27 23:02:02 +0200

    Added Routes and Controllers

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log('04 Store API')
+require("dotenv").config();
+require("express-async-errors");
+const connectBD = require("./db/connect");
+const productsRouter = require("./routes/products");
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+//routes
+app.get("/", (req, res) => {
+  res.send('<h2>Store API</h2> <a href="/api/v1/products">products route</a>');
+});
+
+app.use("/api/v1/products", productsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectBD(process.env.MONGO_URI);
+    app.listen(port, console.log(`app is listening to port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
